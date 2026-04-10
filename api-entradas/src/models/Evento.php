a032245b1b66a0f8df88ff3114f56a108799322b
Marinfinity <mavi.fer2006@gmail.com>
2026-04-10 11:32:39 +0200

    cositas Vicky

--- api-entradas/src/models/Evento.php
+++ api-entradas/src/models/Evento.php
@@ -13,6 +13,14 @@ class Evento {
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+//BUSCAR EVENTOS POR NOMBRE
+    public static function searchByName($pdo, $nombre) {
+        $stmt = $pdo->prepare("SELECT * FROM eventos WHERE nombre LIKE ?");
+        $stmt->execute(['%' . $nombre . '%']);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
 // CREAR NUEVO EVENTO
     public static function create($pdo, $data) {
         $stmt = $pdo->prepare("INSERT INTO eventos (nombre, descripcion, ubicacion, fecha, entradas_totales, entradas_disponibles) VALUES (?, ?, ?, ?, ?, ?)");
